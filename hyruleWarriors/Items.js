8142420373c535fd92502dc417d87b861cf58b5c
Aroc <flo_lenggi@hotmail.com>
2018-07-14 20:41:09 +0200

    added Bulk Edit, Items.js

---
+++ hyruleWarriors/Items.js
@@ -0,0 +1,258 @@
+function LoadItems() {
+	var Items = [
+		{
+ 			Name: {
+ 				EN: 'Compass',
+ 				DE: 'Compass',
+ 				FR: 'Compass',
+ 				ES: 'Compass',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x0,
+		},
+		{
+ 			Name: {
+ 				EN: 'Bombs',
+ 				DE: 'Bombs',
+ 				FR: 'Bombs',
+ 				ES: 'Bombs',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x1,
+		},
+		{
+ 			Name: {
+ 				EN: 'Candle',
+ 				DE: 'Candle',
+ 				FR: 'Candle',
+ 				ES: 'Candle',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x2,
+		},
+		{
+ 			Name: {
+ 				EN: 'Ladder',
+ 				DE: 'Ladder',
+ 				FR: 'Ladder',
+ 				ES: 'Ladder',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x3,
+		},
+		{
+ 			Name: {
+ 				EN: 'Power Bracelet',
+ 				DE: 'Power Bracelet',
+ 				FR: 'Power Bracelet',
+ 				ES: 'Power Bracelet',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x4,
+		},
+		{
+ 			Name: {
+ 				EN: 'Water Bombs',
+ 				DE: 'Water Bombs',
+ 				FR: 'Water Bombs',
+ 				ES: 'Water Bombs',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x5,
+		},
+		{
+ 			Name: {
+ 				EN: 'Digging Mitts',
+ 				DE: 'Digging Mitts',
+ 				FR: 'Digging Mitts',
+ 				ES: 'Digging Mitts',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x6,
+		},
+		{
+ 			Name: {
+ 				EN: 'Ice Arrows',
+ 				DE: 'Ice Arrows',
+ 				FR: 'Ice Arrows',
+ 				ES: 'Ice Arrows',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x7,
+		},
+		{
+ 			Name: {
+ 				EN: 'Raft',
+ 				DE: 'Raft',
+ 				FR: 'Raft',
+ 				ES: 'Raft',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x8,
+		},
+		{
+ 			Name: {
+ 				EN: 'Hookshot',
+ 				DE: 'Hookshot',
+ 				FR: 'Hookshot',
+ 				ES: 'Hookshot',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0x9,
+		},
+		{
+ 			Name: {
+ 				EN: 'Recorder',
+ 				DE: 'Recorder',
+ 				FR: 'Recorder',
+ 				ES: 'Recorder',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0xA,
+		},
+		{
+ 			Name: {
+ 				EN: 'Goddess\'s Harp',
+ 				DE: 'Goddess\'s Harp',
+ 				FR: 'Goddess\'s Harp',
+ 				ES: 'Goddess\'s Harp',
+ 			},
+			Maps: {
+				AdventureMap: true,
+				GreatSeaMap: false,
+				MasterQuestMap: false,
+				MasterWindWakerMap: false,
+				TwilightMap: false,
+				TerminaMap: false,
+				KoholintIslandMap: false,
+				GrandTravelsMap: false,
+				LoruleMap: false,
+				RewardsMap: false
+			},
+			Offset: 0xB,
+		},	
+	]
+	
+	return Items;
+}
--- hyruleWarriors/Offsets.js
+++ hyruleWarriors/Offsets.js
@@ -7,7 +7,17 @@ function LoadOffsets() {
 	Offsets.Characters = {}
 	Offsets.Characters.Base 	= 0x0003308C
 	Offsets.Maps = {}
-	Offsets.Maps.AdventureMap	= 0x00002BE8
+	Offsets.Maps.AdventureMap = {}
+	Offsets.Maps.AdventureMap.Base		= 0x00002BE8
+	Offsets.Maps.AdventureMap.LevelCount = 128
+	Offsets.Maps.AdventureMap.Unlocked	= 0x00002BEC //00 = locked, 01 = beaten, 02 = unlocked, 03 = beaten and unlocked, 4 Bytes [XX 00 00 00]
+	Offsets.Maps.AdventureMap.Beaten	= 0x00002DEC //E0 01 = Beaten, 00 00 = Unbeaten, 8 Bytes [XX XX 00 00 00 00 00 00]
+	Offsets.Maps.AdventureMap.Items		= 0x000031EC
+	Offsets.Maps.AdventureMap.Searched	= 0x00003250 //00 = Not Searched, 01 = Searched, 1 Byte
+	Offsets.Maps.AdventureMap.LevelHasOffser = 0x000032D0 //00 = No Item, 01 = Item, 1 Byte
+	Offsets.Maps.AdventureMap.EnemiesHidden	= 0x00003350 //00 = Hidden, 01 = Visible, 1 Byte
+	Offsets.Maps.AdventureMap.Medal	= 0x000034D0 // FF FF FF FF = Locked/Unbeaten, 01 00 00 00 = Bronze, 02 = Silver, 03 = Gold, 4 Bytes
+	Offsets.Maps.AdventureMap.NoFog	= 0x00005550 // 01 01 01 01 01 01 01 01 = No Fog, 00 00 00 00 00 00 00 00 = Fog, 8 Bytes
 	
 	return Offsets;
 }
