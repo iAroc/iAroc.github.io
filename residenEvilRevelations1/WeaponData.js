4aec4bd55b7e94586fa9541ad768491ef4b2ae80
Aroc <flo_lenggi@hotmail.com>
2018-07-16 03:51:16 +0200

    First Draft

---
+++ residenEvilRevelations1/WeaponData.js
@@ -0,0 +1,133 @@
+function LoadWeaponData() {
+	var WeaponData = [
+		{
+			Name: 'Handgun',
+			Value: 0x10,
+			Types: [
+				{
+					Name: 'M92F',
+					Value: 0x00
+				},
+				{
+					Name: 'Government',
+					Value: 0x10
+				},
+				{
+					Name: 'G18',
+					Value: 0x40
+				},
+				{
+					Name: 'PC356',
+					Value: 0x60
+				},
+				{
+					Name: 'Jill\'s Samurai Edge',
+					Value: 0x70
+				},
+				{
+					Name: 'Jessica\'s G18',
+					Value: 0x80
+				},
+				{
+					Name: 'Parker\'s Government',
+					Value: 0xA0
+				}
+			]		
+		},
+		{
+			Name: 'Magnum',
+			Value: 0x11,
+			Types: [
+				{
+					Name: 'Python',
+					Value: 0x00
+				},
+				{
+					Name: 'Lightning Hawk',
+					Value: 0x10
+				},
+				{
+					Name: 'Pale Rider',
+					Value: 0x20
+				},
+			]		
+		},
+		{
+			Name: 'Machine Gun/SMG',
+			Value: 0x12,
+			Types: [
+				{
+					Name: 'MP5',
+					Value: 0x00
+				},
+				{
+					Name: 'P90',
+					Value: 0x10
+				},
+				{
+					Name: 'AUG',
+					Value: 0x20
+				},
+				{
+					Name: 'G36',
+					Value: 0x30
+				},
+				{
+					Name: 'High Roller',
+					Value: 0x50
+				},
+			]		
+		},
+		{
+			Name: 'Shotgun',
+			Value: 0x13,
+			Types: [
+				{
+					Name: 'Windham',
+					Value: 0x00
+				},
+				{
+					Name: 'M3',
+					Value: 0x10
+				},
+				{
+					Name: 'Hydra',
+					Value: 0x20
+				},
+				{
+					Name: 'Drake',
+					Value: 0x30
+				},
+			]		
+		},
+		{
+			Name: 'Sniper Rifle',
+			Value: 0x14,
+			Types: [
+				{
+					Name: 'M40A1',
+					Value: 0x00
+				},
+				{
+					Name: 'PSG1',
+					Value: 0x10
+				},
+				{
+					Name: 'Muramasa',
+					Value: 0x20
+				},
+			]		
+		},
+		{
+			Name: 'Rocket Launcher',
+			Value: 0x16,
+			Types: [
+				{
+					Name: 'Rocket Launcher',
+					Value: 0x00
+				},
+			]		
+		},
+	]
+	return WeaponData;
+}
